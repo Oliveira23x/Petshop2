5e5aab0b88f3b2ee08b46eff7e81c6621d5274c3
Oliveira23x <diegooliveira01.ez@gmail.com>
2025-08-28 19:15:35 -0300

    Add files via upload

---
+++ Petshop2/Aves.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop2
+{
+    public class Aves : Animal
+    {
+        public Aves(string nome, int idade, string dono, string telefone) : base(nome, idade, dono, telefone) { }
+        public override void Atender()
+        {
+            Console.WriteLine($"Atendendo {nome}! está cantando muito! PIU PIU");
+        }
+    }
+}
